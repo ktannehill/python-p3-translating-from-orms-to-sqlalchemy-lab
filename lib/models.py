c8fd92de8a8a99d01bc6a83d2676d8956edef519
Kat Tannehill <katetannehill@gmail.com>
2023-11-10 20:35:32 -0700

    Complete lab

--- lib/models.py
+++ lib/models.py
@@ -11,3 +11,7 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    def __repr__(self):
+        return f"Dog {self.id}: {self.name}, {self.breed}"
+
